9992890b462f235a28696074ce8bc86706e58297
Lukas Brunner <lukasbrunnerjku@gmx.at>
2021-12-28 15:25:54 +0100

    still not working

--- dr.py
+++ dr.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import torch
 from torch.functional import Tensor
+import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
 from torch.nn.parameter import Parameter
@@ -53,15 +54,25 @@ import pdb
 
 class Discriminator(nn.Module):
     
-    def __init__(
-            self, 
-            nc: int = 1, 
-        ):
-        
-    def forward(self, x):
-        # x is Bx1xHxW
-        
-        
+    def __init__(self):
+        super().__init__()
+        self.submodules = nn.Sequential(
+            nn.Linear(32*32, 16*16),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(16*16, 8*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(8*8, 1),  # Bx1
+        )
+    
+    def forward(self, x):  # H=W=64
+        B = x.shape[0]  # x is Bx1xHxW
+        x = F.interpolate(x, size=(32, 32))  # H=W=32
+        x = x.view(B, -1)  # Bx(H*W)
+        x = self.submodules(x)  # Bx1
+        x = x[:, :, None, None]  # Bx1x1x1; to be consistent with the convolutional discriminator
+        return x
+    
+    
 def weights_init(m):
     # model.apply(weights_init)
     if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
@@ -89,10 +100,11 @@ if __name__ == '__main__':
             raise AttributeError
         
         N = sample.shape[0]
-        lp = -N*z_log_std_source - N/2*math.log(2*math.pi) - ((z - z_mu_source[None, :])**2).sum(0)/(2*torch.exp(z_log_std_source)**2)
+        lp = ( -N*z_log_std_source - N/2*math.log(2*math.pi) - 
+              ((z - z_mu_source[None, :])**2).sum(0)/(2*torch.exp(z_log_std_source)**2) )
         return lp  # 2,
     
-    optG = optim.Adam((z_mu_source, z_log_std_source), lr=0.05, betas=(0.7, 0.999))
+    optG = optim.Adam((z_mu_source, z_log_std_source), lr=0.05, betas=(0.5, 0.999))
     
     # define target gaussian
     z_mu_target = Parameter(torch.tensor([45.0, 49.0]))
@@ -108,7 +120,7 @@ if __name__ == '__main__':
     nparams = sum(p.numel() for p in D.parameters() if p.requires_grad)
     print(f'Discriminator parameters: {nparams/1e3:.3f}k')
     
-    optD = optim.Adam(D.parameters(), lr=0.0005, betas=(0.5, 0.999))
+    optD = optim.Adam(D.parameters(), lr=0.005, betas=(0.7, 0.999))
     
     def render(z: np.ndarray) -> np.ndarray:
         if z.ndim == 2:
@@ -140,8 +152,8 @@ if __name__ == '__main__':
     zeros = torch.zeros((B,1,1,1), dtype=torch.float32)
     ones = torch.ones((B,1,1,1), dtype=torch.float32)
          
-    num_steps = 50
-    warmup_D_steps = 50
+    num_steps = 100
+    warmup_D_steps = 80
     def draw_gaussian(img, mu, std, color):
         mu: Tuple[int] = tuple(map(int, mu))  # 2,
         std: Tuple[int] = tuple(map(int, std))  # 2,
@@ -176,17 +188,10 @@ if __name__ == '__main__':
         if first:
             # test how good the descriminator is
             with torch.no_grad():
-                z = z_mu_source + torch.exp(z_log_std_source) * unorm.sample((B,))  # Bx2
-                x_source = torch.from_numpy(G(z.detach().numpy()))  # Bx1xHxW
-                
-                # sample from the target distribution
-                z = z_mu_target + z_std_target * unorm.sample((B,))  # Bx2
-                x_target = torch.from_numpy(G(z.detach().numpy()))  # Bx1xHxW
-                
-                probs_source = torch.sigmoid(D(x_source)).mean()  # should be low
-                probs_target = torch.sigmoid(D(x_target)).mean()  # should be high
-                print('probs source:', probs_source)
-                print('probs target:', probs_target)
+                nll_source = -torch.log(torch.sigmoid(D(x_source))).sum()  # should be 
+                nll_target = -torch.log(torch.sigmoid(D(x_target))).sum()  # should be 
+                print(f'nll source after {warmup_D_steps} steps of warmup:', nll_source)
+                print(f'nll target after {warmup_D_steps} steps of warmup:', nll_target)
                 
         # the non-saturating GAN loss would be
         # lossG = criterion(D(x_source), ones).mean()
@@ -212,33 +217,36 @@ if __name__ == '__main__':
         lossG = (log_probs * (u - u_ema)).mean()
         lossG.backward()
         
-        print(z_mu_source.grad)
-        pdb.set_trace()
+        # print(z_mu_source.grad)
+        # pdb.set_trace()
         
         optG.step()
         
-        # track training trajectory in weight space
-        mus.append(z_mu_source.detach().numpy())
-        stds.append(torch.exp(z_log_std_source).detach().numpy())
+        # # track training trajectory in weight space
+        # mus.append(z_mu_source.detach().numpy())
+        # stds.append(torch.exp(z_log_std_source).detach().numpy())
         
         if first:  # reduce variance of estimator
-            u_ema = u.mean()
+            u_ema = u.mean(0, keepdims=True)
             first = False
         else:
-            u_ema = alpha * u.mean() + (1 - alpha) * u_ema
+            u_ema = alpha * u.mean(0, keepdims=True) + (1 - alpha) * u_ema
             
     
-    img = cv.cvtColor(canvas.copy(), cv.COLOR_GRAY2RGBA)  # HxWx4
-    for i, (mu, std) in enumerate(zip(mus, stds)):
-        alpha = 0.4 + i/len(mus) * 0.6
-        color = (1, 0, 0, alpha)
-        img = draw_gaussian(img, mu, std, color)
+    # img = cv.cvtColor(canvas.copy(), cv.COLOR_GRAY2RGBA)  # HxWx4
+    # for i, (mu, std) in enumerate(zip(mus, stds)):
+    #     alpha = 0.4 + i/len(mus) * 0.6
+    #     color = (1, 0, 0, alpha)
+    #     img = draw_gaussian(img, mu, std, color)
     
-    mu = z_mu_target.detach().numpy()  # 2,
-    std = z_std_target.detach().numpy()  # 2,
-    img = draw_gaussian(img, mu, std, (0, 1, 0, 1))
+    # mu = z_mu_target.detach().numpy()  # 2,
+    # std = z_std_target.detach().numpy()  # 2,
+    # img = draw_gaussian(img, mu, std, (0, 1, 0, 1))
     
-    cv.imshow('', img)
-    cv.waitKey(0) 
-    cv.destroyAllWindows()
+    # cv.imshow('', img)
+    # cv.waitKey(0) 
+    # cv.destroyAllWindows()
+    
+    print('mu:', z_mu_source.data, 'goal:', z_mu_target.data)
+    print('std:', torch.exp(z_log_std_source.data), 'goal:', z_std_target.data)
     
